dd076a32c1281fdfc2e90c30c57d2de0da6d8829
Shivnath Kisku <124551959+Shivkisku@users.noreply.github.com>
2024-09-20 21:20:03 +0530

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Spinner from '../components/Spinner';
+
+export default function Home() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <Spinner />
+    </div>
+  );
+}
